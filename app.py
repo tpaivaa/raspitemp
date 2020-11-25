6d29884e2c7e0671ff713ee9194ec99b7974dcc4
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-11-25 20:56:49 +0200

    split temp

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ with os.scandir(basepath) as entries:
   for entry in entries:
     if entry.name.startswith('28'):
       with open(basepath + entry.name + '/w1_slave') as f:
-        print(f.read())
+        print(f.read().split('t=')[1])
 
