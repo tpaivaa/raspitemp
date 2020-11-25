9a356e03e0b4107a1b01ef24c807c105e2cf65e3
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-11-25 21:01:03 +0200

    cast to int

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ with os.scandir(basepath) as entries:
   for entry in entries:
     if entry.name.startswith('28'):
       with open(basepath + entry.name + '/w1_slave') as f:
-        print(entry.name + ':' + round(f.read().split('t=')[1]),1)
+        print(entry.name + ':' + round(int(f.read().split('t=')[1])),1)
 
