ac2acf9bcbbc6bf9e8fc078782c03a71c7af575b
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-11-25 21:25:28 +0200

    cleaning

--- app.py
+++ app.py
@@ -9,18 +9,16 @@ def exceptionHandleri(e):
 
 try:
   basepath = '/sys/bus/w1/devices/'
+  sensortemps = {}
   with os.scandir(basepath) as entries:
-    sensortemps = {}
+    
     for entry in entries:
       if entry.name.startswith('28'):
         with open(basepath + entry.name + '/w1_slave') as f:
-          sensortemps = {}
           temp = int(f.read().split('t=')[1])
           temp = round(temp,2)
-          print(entry.name + ':' + str(temp)
-          sensor = entry.name
-          sensortemps[sensor] = str(temp)
-
+          sensortemps[entry.name] = temp
+          print(entry.name + ':' + str(temp))
   print(sensortemps)
 
 except Exception as e:
