a74787d7714e27edb2cb1e34e3646b7a49f66993
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-11-25 21:10:58 +0200

    sensortemps dict

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-
+global sensortemps
+sensortemps = {}
 basepath = '/sys/bus/w1/devices/'
 with os.scandir(basepath) as entries:
   for entry in entries:
     if entry.name.startswith('28'):
       with open(basepath + entry.name + '/w1_slave') as f:
-        print(entry.name + ':' + str(round(int(f.read().split('t=')[1]),2)))
+        temp = int(f.read().split('t=')[1])
+        temp = round(temp,2)
+        print(entry.name + ':' + str(temp)
+        sensortemps[entry.name] = temp
 
+print(sensortemps)
