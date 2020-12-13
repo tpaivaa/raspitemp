625de6f22b4fba7917daf3ddf4a5217a30241e60
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-12-14 00:54:39 +0200

    pinging

--- WSClientapp.py
+++ WSClientapp.py
@@ -8,8 +8,12 @@ async def hello():
       async with websockets.connect(uri) as websocket:
         while True:
           message = ''
+          ping = json.dumps({'ping':1})
           try:
             async for message in websocket:
+              
+              await websocket.send(ping)
+              await websocket.recv()
               data = json.loads(message)
               print(data)
           except asyncio.TimeoutError:
